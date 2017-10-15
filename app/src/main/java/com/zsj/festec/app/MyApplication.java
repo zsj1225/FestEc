d73da39b7e88a9bf04e54b741360c69bb5980d05
zhushengjun <1185335441@qq.com>
2017-10-15 22:23:39 +0800

    项目架构初始化

---
+++ app/src/main/java/com/zsj/festec/app/MyApplication.java
@@ -0,0 +1,24 @@
+package com.zsj.festec.app;
+
+import android.app.Application;
+
+import com.zsj.lib.androidlib.app.app.Latte;
+
+/**
+ * 创建者     朱胜军
+ * 创建时间   2017/10/15 22:22
+ * 描述	      TODO
+ * <p>
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   TODO
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Latte.init(this)
+                .withApiHost("")
+                .configure();
+    }
+}
